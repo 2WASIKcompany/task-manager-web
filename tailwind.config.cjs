34f839e91b1119fe77bb289910d134ec217780fe
Timkorn <timadzr@gmail.com>
2022-12-13 00:23:02 +0300

    add: tailwind + react query

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "assent-color": "#286EF1",
+        "bg-darkest": "#17171A",
+        "bg-lighter": "#1E1E29",
+        "border-light-blue": "#282865",
+      },
+    },
   },
   plugins: [],
 };
